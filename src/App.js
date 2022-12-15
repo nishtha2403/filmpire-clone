6796dea19dea83e6334d76d6f7f8236732b7aa25
Nishtha Sharma <nishtha.24sharma1998@gmail.com>
2022-12-15 15:30:25 +0530

    MovieLand
    A simple React Project to search for movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
+import MovieCard from './MovieCard';
+
+import  './App.css';
+import SearchIcon from './search.svg';
+ 
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=fb205972';
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    },[]);
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {
+                movies.length > 0 ? 
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard key={movie.imdbID} movie={movie}/>
+                        ))}
+                    </div>
+                : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
